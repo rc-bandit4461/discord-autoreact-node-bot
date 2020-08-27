56cc7fca8717b01dafe7490b6fe8434b34acff3f
Aymane Boubleh <ayman.boubleh@gmail.com>
2020-08-27 08:06:43 +0100

    initial commit

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
-//? "NzQ4MzU2NjEzMjMyMDAxMDQ1.X0cPaA.IVDyaE9pHp6pLvI6F3Bzp30E-NM"
-//? https://discord.com/api/oauth2/authorize?client_id=748356613232001045&permissions=8&scope=bot
 const Discord = require("discord.js");
 const config = require("./config.json");
 const bot = new Discord.Client();
-
+//reacts to the user message with the emojis present in config.user.emojis  
 function global_react(message,user){
-    console.log("GLOBAL REACT");
+    // console.log("GLOBAL REACT");
     for(const emoji_id of user.emojis){
         const emoji = message.guild.emojis.cache.get(emoji_id);
         if(emoji != undefined)
         message.react(emoji);
     }
 }
+//reacts to the user message with the emojis present in config.user.reactions
+// if a pattern has been found, the reactions associated with it will take place  
 function pattern_react(message,user){
-    console.log("PATTERN REACT");
+    // console.log("PATTERN REACT");
     for(reaction of user.reactions){
         for(pattern of reaction.patterns) if (message.content.toLowerCase().includes(pattern)) {
             for(const emoji_id of reaction.emojis){
@@ -30,16 +30,12 @@ bot.on("ready", function () {
 });
 
 bot.on("message", (message) => {
-    // console.log(message.content);
-    // console.log(message.guild.emojis.cache);
-    // return;
     for (const user of config.users) {
         if (user.id != message.author.id) continue;
         if (user.to_all_channels || user.channels.includes(message.channel.id)) {
             if (user.global_pattern == true) global_react(message, user);
             if(user.special_patterns == true) pattern_react(message,user);
-        }
-        
+        }        
         return;
     }
 });
